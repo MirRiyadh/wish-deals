e8fb9c6ecffb6ff0b836c719a15882dcf632f604
Riyadh <riyadh.ju@gmail.com>
2022-11-24 20:36:36 +0600

    added section part

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
+import AboutUs from "./AboutUs/AboutUs";
 import IconsSection from "./IconsSection/IconsSection";
+import NewsLetters from "./NewsLetters/NewsLetters";
 import ShopCategories from "./ShopCategories/ShopCategories";
 import Slider from "./Slider/Slider";
 
@@ -9,6 +11,8 @@ const Home = () => {
       <Slider></Slider>
       <IconsSection></IconsSection>
       <ShopCategories></ShopCategories>
+      <AboutUs></AboutUs>
+      <NewsLetters></NewsLetters>
     </div>
   );
 };
---
+++ src/pages/Home/NewsLetters/NewsLetters.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewsLetters = () => {
+  return <div>letter</div>;
+};
+
+export default NewsLetters;
